17b4f7aa034259bcbe1cfb5a5fb796e189117648
DominicTuo <2509861430@qq.com>
2022-04-08 09:23:10 +0800

    项目初始化，引入Vant

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
     <h1>移动项目</h1>
+    <i class="toutiao toutiao-yuedu"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-
-}
+  name: "App",
+};
 </script>
 
 <style scoped lang="less">
-
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/styles/index.less'  //引入全局样式
+import Vant from 'vant';  //引入vant
+import 'vant/lib/index.css';
+
+Vue.use(Vant);  //挂载Vant
 
 Vue.config.productionTip = false
 
